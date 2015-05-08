2d9103ffce1d4a06fb203a6b3c566775923df4bc
Manny Jois <m.k.jois@gmail.com>
2015-05-08 02:19:42 -0700

    track school directory using env var

--- bash/mycd.sh
+++ bash/mycd.sh
@@ -3,12 +3,12 @@
 cd() {
     if [ $# -eq 0 ]; then
         builtin cd ~
-    elif [ -d ~/learn/cs$1 ]; then
-        builtin cd ~/learn/cs$1
-    elif [ -d ~/learn/ee$1 ]; then
-        builtin cd ~/learn/ee$1
-    elif [ -d ~/learn/er$1 ]; then
-        builtin cd ~/learn/er$1
+    elif [ -d $UNIVDIR/cs$1 ]; then
+        builtin cd $UNIVDIR/cs$1
+    elif [ -d $UNIVDIR/ee$1 ]; then
+        builtin cd $UNIVDIR/ee$1
+    elif [ -d $UNIVDIR/er$1 ]; then
+        builtin cd $UNIVDIR/er$1
     else
         builtin cd $1
     fi
