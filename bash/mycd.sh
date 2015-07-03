5bd051a897cec2029acd3c3995b13fce6706dfba
Manny Jois <m.k.jois@gmail.com>
2015-07-03 16:20:45 -0700

    mycd now handles files with spaces

--- bash/mycd.sh
+++ bash/mycd.sh
@@ -3,13 +3,13 @@
 cd() {
     if [ $# -eq 0 ]; then
         builtin cd ~
-    elif [ -d $UNIVDIR/cs$1 ]; then
-        builtin cd $UNIVDIR/cs$1
-    elif [ -d $UNIVDIR/ee$1 ]; then
-        builtin cd $UNIVDIR/ee$1
-    elif [ -d $UNIVDIR/er$1 ]; then
-        builtin cd $UNIVDIR/er$1
+    elif [ -d "$UNIVDIR/cs$1" ]; then
+        builtin cd "$UNIVDIR/cs$1"
+    elif [ -d "$UNIVDIR/ee$1" ]; then
+        builtin cd "$UNIVDIR/ee$1"
+    elif [ -d "$UNIVDIR/er$1" ]; then
+        builtin cd "$UNIVDIR/er$1"
     else
-        builtin cd $1
+        builtin cd "$1"
     fi
 }
